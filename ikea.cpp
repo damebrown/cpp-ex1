ef1c583ae220725e5adc9f68cf01e91d5235b6dc
Dani Mench Brown <33936201+damebrown@users.noreply.github.com>
2018-09-03 18:20:53 +0300

    Add files via upload

---
+++ ikea.cpp
@@ -0,0 +1,5 @@
+//
+// Created by daniel.brown1 on 9/3/18.
+//
+
+#include "ikea.h"
---
+++ ikea.h
@@ -0,0 +1,15 @@
+//
+// Created by daniel.brown1 on 9/3/18.
+//
+
+#ifndef EX2_IKEA_H
+#define EX2_IKEA_H
+
+
+class ikea
+{
+
+};
+
+
+#endif //EX2_IKEA_H
